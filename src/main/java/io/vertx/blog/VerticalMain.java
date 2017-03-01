43ff4120a973ee0b832c024e48f54f4fbe40d7c0
King <jxk4066@d25sy0fbgq17im.amer.homedepot.com>
2017-03-01 12:37:13 -0500

    init commit

---
+++ src/main/java/io/vertx/blog/VerticalMain.java
@@ -0,0 +1,7 @@
+package io.vertx.blog;
+
+/**
+ * Created by jxk4066 on 3/1/17.
+ */
+public class VerticalMain {
+}
---
+++ src/main/java/io/vertx/blog/first/MyFirstVerticle.java
@@ -0,0 +1,61 @@
+package io.vertx.blog.first;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.StaticHandler;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MyFirstVerticle extends AbstractVerticle {
+    // Store our product
+    private Map<Integer, Whisky> products = new LinkedHashMap<>();
+
+    @Override
+    public void start(Future<Void> fut) {
+        Router router = Router.router(vertx);
+
+        createSomeData();
+
+        router.route("/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.putHeader("context/type", "text/html")
+                    .end("<h1>Hello from my first Vert.x 3 application</h1>");
+        });
+
+        // Serve static resources from the /assets directory
+        router.route("/assets/*").handler(StaticHandler.create("assets"));
+        router.get("/api/whiskies").handler(this::getAll);
+
+
+        vertx.createHttpServer()
+                .requestHandler(router::accept)
+                .listen(
+                        config().getInteger("http.port", 8080),
+                        result -> {
+                            if (result.succeeded()) {
+                                fut.complete();
+                            } else {
+                                fut.fail(result.cause());
+                            }
+                        });
+    }
+
+    private void getAll(RoutingContext routingContext) {
+        routingContext.response()
+                .putHeader("content-type", "application/json; charset=utf-8")
+                .end(Json.encodePrettily(products.values()));
+    }
+
+    // Create some product
+    private void createSomeData() {
+        Whisky bowmore = new Whisky("Bowmore 15 Years Laimrig", "Scotland, Islay");
+        products.put(bowmore.getId(), bowmore);
+        Whisky talisker = new Whisky("Talisker 57° North", "Scotland, Island");
+        products.put(talisker.getId(), talisker);
+    }
+}
